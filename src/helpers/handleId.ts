8ef0c89a1030e14dbf47f80438b7d92061360694
backmeupplz <backmeupplz@gmail.com>
2021-04-30 09:49:04 -0700

    fix handle id

--- src/helpers/handleId.ts
+++ src/helpers/handleId.ts
@@ -3,6 +3,9 @@ import { ExtraReplyMessage } from 'telegraf/typings/telegram-types'
 import { checkCAS } from '@/helpers/cas'
 
 export async function handleId(ctx: Context) {
+  if (ctx.chat?.type !== 'private') {
+    return
+  }
   const userId = +ctx.message.text.match(/-?\d+/g)[0]
   return ctx.reply(
     await checkCAS(userId),
